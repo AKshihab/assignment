41f1de37bfdad19b8f99a5f4c925cf10d6e7f469
AKshihab <shihabkaiyumhossen@gmail.com>
2024-09-29 18:39:36 +0600

    input output

---
+++ assignment number-12.cpp
@@ -0,0 +1,13 @@
+using namespace std;
+int sum(int a, int b , int c){
+    return a+b+c;
+}
+
+int main() {
+      int a;
+      int b;
+      int c;
+      cin>>a >>b >> c;
+      cout <<sum(a,b,c);
+    return 0;
+}
